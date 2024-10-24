b2e0a3600fc15fd30309924590d3c867d55a80cf
osamadanat <osama.zulfiqar@danatonline.com>
2024-10-24 13:12:11 +0500

    remove alert

--- src/hooks/useScrollbarVisibility.js
+++ src/hooks/useScrollbarVisibility.js
@@ -5,7 +5,6 @@ const useScrollbarVisibility = (isVisible) => {
   useEffect(() => {
     const handleScrollbar = () => {
       if (isVisible) {
-        alert('ok')
         document.body.style.overflow = 'hidden';
       } else {
         document.body.style.overflow = 'visible';
