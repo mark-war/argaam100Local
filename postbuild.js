fd3429ecb74f3303ab3150818a1406d71543ff76
Mark Angelo Guerra <14197001+mark-war@users.noreply.github.com>
2025-02-03 11:57:29 +0400

    Rewrite Action URL

--- postbuild.js
+++ postbuild.js
@@ -1,6 +1,7 @@
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import fs from "fs-extra";
+import injectActionUrl from "./injectActionUrl.js";
 
 // Get the directory name of the current module
 const __filename = fileURLToPath(import.meta.url);
@@ -8,10 +9,6 @@ const __dirname = dirname(__filename);
 
 // Define source and destination directories
 const sourceDir = `${__dirname}/dist`;
-// const enDir = `${__dirname}/dist/en/screener`;
-// const arDir = `${__dirname}/dist/ar/screener`;
-// const en10Dir = `${__dirname}/dist/en/top-10`;
-// const ar10Dir = `${__dirname}/dist/ar/top-10`;
 const en100Dir = `${__dirname}/dist/en/argaam-100`;
 const ar100Dir = `${__dirname}/dist/ar/argaam-100`;
 
@@ -19,10 +16,6 @@ const ar100Dir = `${__dirname}/dist/ar/argaam-100`;
 const mode = process.env.BUILD_MODE || "development";
 
 // Create language-specific directories and subdirectories if they don't exist
-// fs.ensureDirSync(enDir);
-// fs.ensureDirSync(arDir);
-// fs.ensureDirSync(en10Dir);
-// fs.ensureDirSync(ar10Dir);
 fs.ensureDirSync(en100Dir);
 fs.ensureDirSync(ar100Dir);
 
@@ -35,14 +28,13 @@ fs.readdir(sourceDir, (err, files) => {
 
     if (fs.statSync(srcPath).isFile()) {
       // Copy files differently based on the environment mode
-      // fs.copyFileSync(srcPath, `${enDir}/${file}`);
-      // fs.copyFileSync(srcPath, `${arDir}/${file}`);
-      // fs.copyFileSync(srcPath, `${en10Dir}/${file}`);
-      // fs.copyFileSync(srcPath, `${ar10Dir}/${file}`);
       fs.copyFileSync(srcPath, `${en100Dir}/${file}`);
       fs.copyFileSync(srcPath, `${ar100Dir}/${file}`);
     }
   });
+
+  // inject the action URL
+  injectActionUrl();
 
   console.log(
     "Build files organized into language-specific folders with subfolders."
